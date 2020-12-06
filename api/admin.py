c48174c5ab63a4fcea8df9f8ce112509534831b3
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2020-12-06 17:53:26 +0900

    "Command"APIを作成し、管理者画面に登録
    Modelに"Command"を追加し、管理者画面に表示されるようにした。
    ※0001_initial.pyは登録時の自動生成ファイル。

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Command
 
 # Register your models here.
+
+admin.site.register(Command)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Command(models.Model):
+    cmd = models.TextField()
